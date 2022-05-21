a887ec817c15ed3dba563075625f1d5aa68111f4
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-05-21 00:55:10 -0400

    Re-structured. Removed login_auth. Working Routes

--- media/routes.py
+++ media/routes.py
@@ -1,17 +1,21 @@
 """ File For the Endpoints """
 
+from requests import session
 from media import app
 from media.credentials_info import SECRET_CLIENT_ID, SECRET_KEY
-from flask import render_template, redirect, url_for, request
-from media.login_auth import make_spotify_oauth
+from flask import render_template, redirect, url_for, request, session
+from spotipy.oauth2 import SpotifyOAuth
 
+
+# A Key within this a session
+SESSION_TOKEN = "session-token"
 
 # Endpoint for Home Page
 @app.route('/')
 @app.route('/home')
 def home_page():
     # TODO: Render home.html
-    return "Ansh Home"
+    return render_template("home.html")
 
 
 @app.route('/login')
@@ -23,7 +27,12 @@ def login_page():
 
 @app.route('/redirect')
 def authorization_page():
-    return "authorization"
+    sp_ouath = make_spotify_oauth(SECRET_CLIENT_ID, SECRET_KEY)
+    session.clear()
+    request_code = request.args.get('code')
+    session_token = sp_ouath.get_access_token(request_code)
+    session[SESSION_TOKEN] = session_token
+    return redirect(url_for("home_page", _external=True))
 
 
 @app.route('/myTracks')
@@ -31,4 +40,13 @@ def tracks_page():
     return "List of my Tracks"
 
 
-
+def make_spotify_oauth(secret_client_id: str, secret_key: str):
+    """
+    Takes in Two parameters: secret_client, and secret_key that are associated with
+    the app on the spotify developer build.
+    """
+    return SpotifyOAuth(client_id=secret_client_id,
+                        client_secret=secret_key,
+                        redirect_uri=url_for(
+                            'authorization_page', _external=True),
+                        scope="user-library-read")
