3028caa7b92272a23385447ca6b7c11fd9cf3797
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2022-05-27 21:00:47 -0400

    Changes

--- media/spotify_api.py
+++ media/spotify_api.py
@@ -2,19 +2,19 @@
     Class for the Spotify API 
 """
 from __future__ import annotations
+from typing import Any
 
 from requests import request, session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from typing import *
 from credentials_info import SECRET_CLIENT_ID, SECRET_KEY
 from flask import url_for, request
 
-
-# Global Variable for the SESSION_TOKEN 
+# Global Variable for the SESSION_TOKEN
 SESSION_TOKEN = "session-token"
 VALID_ROUTES = ["home_page", "login_page", "authorization_page", "tracks_page", "albums_page"]
 SESSION_TOKEN = "session-token"
+
 
 class SpotifyAPI:
     """
@@ -29,7 +29,7 @@ class SpotifyAPI:
         - redirect_url: the route to be redirected to. 
         - scope: the data that needs to be requested. 
     """
-    auth_manager: SpotifyOAuth
+    auth_manager: Any
     client_id: str
     client_secret_key: str
     session_token: str
@@ -40,28 +40,30 @@ class SpotifyAPI:
         self.auth_manager = None
         self.client_id = SECRET_CLIENT_ID
         self.client_secret_key = SECRET_KEY
-        self.redirect_url = None
-        self.scope = None
-        self.session_token = None
-        session
-    
+        self.redirect_url = ""
+        self.scope = ""
+        self.session_token = ""
+
     def create_spotify_oauth(self, redirect_to_url: str, request_for: str) -> None:
         """
-            Creates a SpotifyOAuth Object that takes in the redirect route and the data to be requested. 
+        Creates a SpotifyOAuth Object that takes in the redirect route and the data to be requested.
 
             Preconditions: 
                 - redirected_to_url must be a valid route
         """
-        
-        if redirect_to_url in VALID_ROUTES: 
+
+        if redirect_to_url in VALID_ROUTES:
             self.auth_manager = SpotifyOAuth(client_id=self.client_id,
-                                        client_secret=self.client_secret_key,
-                                        redirect_uri=url_for(redirect_to_url, _external=True),
-                                        scope=request_for)
-        else: 
+                                             client_secret=self.client_secret_key,
+                                             redirect_uri=url_for(redirect_to_url, _external=True),
+                                             scope=request_for)
+        else:
             raise ValueError
-    
+
     def generate_new_session(self) -> None:
+        """
+        Creates a new session for the user.
+        """
         req_code = request.args.get('code')
         self.session_token = self.auth_manager.get_access_token(req_code)
         session[SESSION_TOKEN] = self.session_token
